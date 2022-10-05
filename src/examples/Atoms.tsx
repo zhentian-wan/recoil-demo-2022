7ea0b7713194bcb5f37ac2dc8119b15111c81dd2
Zhentian Wan <zwan@zscaler.com>
2022-10-05 15:34:34 +0300

    feat: basic

---
+++ src/examples/Atoms.tsx
@@ -0,0 +1,35 @@
+import {atom, useRecoilState, useRecoilValue} from 'recoil'
+
+const darkModeAtom = atom({
+    key: 'darkMode',
+    default: false,
+})
+
+const DarkModeSwitch = () => {
+    const [darkMode, setDarkMode] = useRecoilState(darkModeAtom)
+    return (
+        <input type="checkbox" checked={darkMode} onChange={({currentTarget}) => setDarkMode(currentTarget.checked)} />
+    )
+}
+
+const Button = () => {
+    const darkMode = useRecoilValue(darkModeAtom)
+    return (
+        <button style={{backgroundColor: darkMode ? 'black' : 'white', color: darkMode ? 'white' : 'black'}}>
+            My UI Button
+        </button>
+    )
+}
+
+export const Atoms = () => {
+    return (
+        <>
+            <div>
+                <DarkModeSwitch />
+            </div>
+            <div>
+                <Button />
+            </div>
+        </>
+    )
+}
